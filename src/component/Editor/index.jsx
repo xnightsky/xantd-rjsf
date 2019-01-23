b01f5cad56d413c2ef3d979083108947d1737a00
xia.tian <xia.tian@immomo.com>
2019-01-23 22:56:32 +0800

    feature: editor wrapper

---
+++ src/component/Editor/index.jsx
@@ -0,0 +1,29 @@
+import _ from "lodash";
+import React from "react";
+import MonacoEditor from "react-monaco-editor";
+
+
+export default function Editor(
+  {
+    value,
+    ...restProps
+  }
+) {
+  return (
+    <MonacoEditor
+      width="100%"
+      height="100%"
+      language="json"
+      theme="vs"
+      value={
+        "object" === typeof value ? JSON.stringify(
+          value,
+          null,
+          2
+        ) : value
+      }
+      onChange={null}
+      {...restProps}
+    />
+  );
+}
